83aecb431382b6ea304f74cabe4a80cb73cbc116
Yuchen <tangyuchen218@hotmail.com>
2017-05-01 15:21:59 -0700

    Finished Chapter3 Programming exercise

---
+++ Chapter3Programming/Chapter3_P_6.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+//3.2 addfrac.c
+/*
+int main (int argc, char const *argv[])
+{
+	int num1,denom1,num2,denom2,result_num,result_denom;
+	printf("Enter first fraction: ");
+	scanf("%d/%d",&num1,&denom1);
+	printf("Enter second fraction: ");
+	scanf("%d/%d",&num2,&denom2);
+	
+	result_num = num1 * denom2 + num2 * denom1;
+	result_denom = denom1 * denom2;
+	printf("The sum is %d/%d\n", result_num, result_denom);
+	return 0;
+}*/
+
+//edited addfrac.c
+
+int main (int argc, char const *argv[])
+{
+	int num1,denom1,num2,denom2,result_num,result_denom;
+	printf("Enter two fraction seperated by plus: (a/b+c/d)");
+	scanf("%d/%d+%d/%d",&num1,&denom1,&num2,&denom2);
+	result_num = num1 * denom2 + num2 * denom1;
+	result_denom = denom1 * denom2;
+	printf("The sum is %d/%d\n", result_num, result_denom);
+	return 0;
+}
